4b05234552a30641b5bb6821e525b0e6c2ab1a04
Pedro Comis <comispedro95@gmail.com>
2026-04-08 09:51:22 -0300

    atualização03

---
+++ atividade06.php
@@ -0,0 +1,18 @@
+<?php 
+$numero = 7;
+
+echo "Tabuada do $nomedavariavel:<br>";
+Echo "$numero x 1 = " . ($numero * 1) . "<br>";
+echo  "$numero x 2 = " . ($numero * 2) . "<br>";
+Echo "$numero x 3 = " . ($numero * 3) . "<br>";
+Echo "$numero x 4 = " . ($numero * 4) . "<br>";
+echo  "$numero x 5 = " . ($numero * 5) . "<br>";
+Echo "$numero x 6 = " . ($numero * 6) . "<br>";
+echo  "$numero x 7 = " . ($numero * 7) . "<br>";
+Echo "$numero x 8 = " . ($numero * 8) . "<br>";
+Echo "$numero x 9 = " . ($numero * 9) . "<br>";
+echo  "$numero x 10 = " . ($numero * 10) . "<br>";
+
+
+
+?>
